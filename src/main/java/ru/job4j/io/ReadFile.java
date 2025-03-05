76da654695173a078df4f9e22a1e947888adfb24
devvk <info@sekretov.net>
2025-03-05 16:37:35 +0100

    5.1. BufferedReader [#252489]

---
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -0,0 +1,14 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try (BufferedReader input = new BufferedReader(new FileReader("data/input.txt"))) {
+            input.lines().forEach(System.out::println);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
