ce780d41a2d3879d71854b4ff6a31ce9f6c60ad2
devvk <info@sekretov.net>
2025-05-01 19:33:12 +0200

    Транзакции

---
+++ db/transactions.sql
@@ -0,0 +1,11 @@
+BEGIN;
+UPDATE accounts SET balance = balance - 100.00
+    WHERE name = 'Alice';
+SAVEPOINT my_savepoint;
+UPDATE accounts SET balance = balance + 100.00
+    WHERE name = 'Bob';
+-- ошибочное действие... забыть его и использовать счёт Уолли
+ROLLBACK TO my_savepoint;
+UPDATE accounts SET balance = balance + 100.00
+    WHERE name = 'Wally';
+COMMIT;
