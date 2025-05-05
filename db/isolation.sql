6a1a473a6df18e15cddf4779fda13d664e188ae8
devvk <info@sekretov.net>
2025-05-05 20:11:35 +0200

    Read committed

--- db/isolation.sql
+++ db/isolation.sql
@@ -7,11 +7,65 @@ SET transaction_isolation = 'serializable';
 SHOW transaction_isolation;
 
 -- Уровень изоляции для текущей транзакции.
--- BEGIN ISOLATION LEVER READ UNCOMMITTED;
--- BEGIN ISOLATION LEVER READ COMMITTED;
--- BEGIN ISOLATION LEVER REPEATABLE READ;
--- BEGIN ISOLATION LEVER READ SERIALIZABLE;
+-- BEGIN ISOLATION LEVEL READ UNCOMMITTED;
+-- BEGIN ISOLATION LEVEL READ COMMITTED;
+-- BEGIN ISOLATION LEVEL REPEATABLE READ;
+-- BEGIN ISOLATION LEVEL SERIALIZABLE;
 BEGIN;
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 -- другие операции в рамках транзакции
 COMMIT;
+
+CREATE TABLE products (
+    id       SERIAL PRIMARY KEY,
+    name     VARCHAR(50),
+    producer VARCHAR(50),
+    count    INT DEFAULT 0,
+    price    INT
+);
+
+INSERT INTO products (name, producer, count, price) VALUES ('product_1', 'producer_1', 3, 50);
+INSERT INTO products (name, producer, count, price) VALUES ('product_2', 'producer_2', 15, 32);
+INSERT INTO products (name, producer, count, price) VALUES ('product_3', 'producer_3', 8, 115);
+
+/*
+    Read Committed (чтение фиксированных данных)
+    Аномалия Non-repeatable Read (неповторяющееся чтение):
+    Транзакция читает данные, и в процессе выполнения транзакции
+    эти данные изменяются другой транзакцией.
+*/
+-- Транзакция 1
+BEGIN;
+SELECT * FROM products;
+
+-- Транзакция 2
+BEGIN;
+SELECT * FROM products;
+
+-- Транзакция 1
+INSERT INTO products (name, count, price) VALUES ('product_4', 11, 64);
+DELETE FROM products WHERE price = 115;
+UPDATE products SET price = 75 WHERE name = 'product_1';
+
+-- Транзакция 2 (аномалия dirty read отсутствует)
+SELECT * FROM products;
+
+-- Транзакция 1
+COMMIT;
+
+-- Транзакция 2
+-- Видно аномалию Non-repeatable Read (неповторяющееся чтение): INSERT и DELETE
+-- Видно аномалию Phantom Read (фантомное чтение): INSERT
+SELECT * FROM products;
+
+/*
+    Repeatable Read (повторяющееся чтение)
+    Аномалия Phantom Read (фантомное чтение):
+    Транзакция читает набор строк, и в процессе выполнения транзакции
+    другая транзакция вставляет/удаляет строки, что влияет на набор данных.
+*/
+
+-- Транзакция 1
+
+
+-- Транзакция 2
