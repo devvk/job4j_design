f508e3ffcd0194af707145723e8c5587b84e0c8e
devvk <info@sekretov.net>
2025-05-05 17:23:10 +0200

    Isolation

--- db/isolation.sql
+++ db/isolation.sql
@@ -1,13 +1,17 @@
--- Уровень изоляции по умолчанию
+-- Уровень изоляции по умолчанию.
 SET default_transaction_isolation = 'repeatable read';
 SHOW default_transaction_isolation;
 
--- Уровень изоляции для текущей сессии
+-- Уровень изоляции для текущей сессии.
 SET transaction_isolation = 'serializable';
 SHOW transaction_isolation;
 
--- Уровень изоляции для конкретной транзакции
-SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+-- Уровень изоляции для текущей транзакции.
+-- BEGIN ISOLATION LEVER READ UNCOMMITTED;
+-- BEGIN ISOLATION LEVER READ COMMITTED;
+-- BEGIN ISOLATION LEVER REPEATABLE READ;
+-- BEGIN ISOLATION LEVER READ SERIALIZABLE;
 BEGIN;
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 -- другие операции в рамках транзакции
 COMMIT;
