2684563a6d880fef6bab54e3a5a132e876be8d96
devvk <info@sekretov.net>
2025-12-30 21:35:18 +0100

    2. Парковка машин [#853 #556587]

--- src/main/java/ru/job4j/ood/lsp/parking/CityParking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/CityParking.java
@@ -1,6 +1,7 @@
 package ru.job4j.ood.lsp.parking;
 
 import java.util.Objects;
+import java.util.function.Predicate;
 
 public class CityParking implements Parking {
 
@@ -18,87 +19,116 @@ public class CityParking implements Parking {
     @Override
     public boolean park(Vehicle vehicle) {
         Objects.requireNonNull(vehicle, "Vehicle must not be null");
-        return vehicle.size() == 1 ? parkCar(vehicle) : parkTruck(vehicle);
-    }
-
-    private boolean parkCar(Vehicle vehicle) {
-        for (int i = 0; i < cars.length; i++) {
-            if (cars[i] == null) {
-                cars[i] = vehicle;
-                return true;
-            }
+        int vehicleSize = vehicle.size();
+        if (vehicleSize < 1) {
+            throw new IllegalArgumentException("Vehicle size must be >= 1");
         }
-        return false;
-    }
-
-    private boolean parkTruck(Vehicle vehicle) {
-        for (int i = 0; i < trucks.length; i++) {
-            if (trucks[i] == null) {
-                trucks[i] = vehicle;
-                return true;
-            }
-        }
-        int startIndex = -1;
-        int counter = 0;
-        for (int i = 0; i < cars.length; i++) {
-            if (cars[i] == null) {
-                if (counter == 0) {
-                    startIndex = i;
-                }
-                if (++counter == vehicle.size()) {
-                    for (int j = startIndex; j < startIndex + vehicle.size(); j++) {
-                        cars[j] = vehicle;
-                    }
-                    return true;
-                }
-            } else {
-                counter = 0;
-                startIndex = -1;
-            }
-        }
-        return false;
+        return vehicleSize == 1 ? parkCar(vehicle) : parkTruck(vehicle);
     }
 
     @Override
     public boolean unpark(Vehicle vehicle) {
         Objects.requireNonNull(vehicle, "Vehicle must not be null");
-        return vehicle.size() == 1 ? unparkCar(vehicle) : unparkTruck(vehicle);
+        int vehicleSize = vehicle.size();
+        if (vehicleSize < 1) {
+            throw new IllegalArgumentException("Vehicle size must be >= 1");
+        }
+        return vehicleSize == 1 ? unparkCar(vehicle) : unparkTruck(vehicle);
     }
 
-    private boolean unparkCar(Vehicle vehicle) {
-        for (int i = 0; i < cars.length; i++) {
-            if (cars[i] == vehicle) {
-                cars[i] = null;
-                return true;
+    private int findFirstNullIndex(Vehicle[] slots) {
+        for (int i = 0; i < slots.length; i++) {
+            if (slots[i] == null) {
+                return i;
             }
+        }
+        return -1;
+    }
+
+    private boolean parkCar(Vehicle vehicle) {
+        int idx = findFirstNullIndex(cars);
+        if (idx != -1) {
+            cars[idx] = vehicle;
+            return true;
         }
         return false;
     }
 
-    private boolean unparkTruck(Vehicle vehicle) {
-        for (int i = 0; i < trucks.length; i++) {
-            if (trucks[i] == vehicle) {
-                trucks[i] = null;
-                return true;
+    private int findFirstVehicleIndex(Vehicle[] slots, Vehicle vehicle) {
+        for (int i = 0; i < slots.length; i++) {
+            if (slots[i] == vehicle) {
+                return i;
             }
         }
+        return -1;
+    }
+
+    private boolean unparkCar(Vehicle vehicle) {
+        int idx = findFirstVehicleIndex(cars, vehicle);
+        if (idx != -1) {
+            cars[idx] = null;
+            return true;
+        }
+        return false;
+    }
+
+    private int findConsecutiveStartIndex(Vehicle[] slots, int needed, Predicate<Vehicle> matcher) {
+        if (needed <= 0) {
+            return -1;
+        }
+
         int startIndex = -1;
         int counter = 0;
-        for (int i = 0; i < cars.length; i++) {
-            if (cars[i] == vehicle) {
+        for (int i = 0; i < slots.length; i++) {
+            if (matcher.test(slots[i])) {
                 if (counter == 0) {
                     startIndex = i;
                 }
-                if (++counter == vehicle.size()) {
-                    for (int j = startIndex; j < startIndex + vehicle.size(); j++) {
-                        cars[j] = null;
-                    }
-                    return true;
+                if (++counter == needed) {
+                    return startIndex;
                 }
             } else {
                 counter = 0;
                 startIndex = -1;
             }
+        }
+        return -1;
+    }
+
+    private boolean parkTruck(Vehicle vehicle) {
+        int idx = findFirstNullIndex(trucks);
+        if (idx != -1) {
+            trucks[idx] = vehicle;
+            return true;
+        }
+
+        int vehicleSize = vehicle.size();
+        int startIndex = findConsecutiveStartIndex(cars, vehicleSize, Objects::isNull);
+        if (startIndex != -1) {
+            for (int j = startIndex; j < startIndex + vehicleSize; j++) {
+                cars[j] = vehicle;
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean unparkTruck(Vehicle vehicle) {
+        int idx = findFirstVehicleIndex(trucks, vehicle);
+        if (idx != -1) {
+            trucks[idx] = null;
+            return true;
+        }
+
+        int vehicleSize = vehicle.size();
+        Predicate<Vehicle> isThisTruck = slot -> slot == vehicle;
+        int startIndex = findConsecutiveStartIndex(cars, vehicleSize, isThisTruck);
+        if (startIndex != -1) {
+            for (int j = startIndex; j < startIndex + vehicleSize; j++) {
+                cars[j] = null;
+            }
+            return true;
         }
         return false;
     }
