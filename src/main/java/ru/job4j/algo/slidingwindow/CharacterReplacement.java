8e8597e3e909b3628d53fd2d83800431a0919354
devvk <info@sekretov.net>
2026-02-23 08:48:43 +0100

    CharacterReplacement

--- src/main/java/ru/job4j/algo/slidingwindow/CharacterReplacement.java
+++ src/main/java/ru/job4j/algo/slidingwindow/CharacterReplacement.java
@@ -18,13 +18,12 @@ public class CharacterReplacement {
      */
     public static int characterReplacement(String s, int k) {
         int[] freq = new int[26];
-        int left = 0;
-        int maxFreqChar = 0;
-        int bestLen = 0;
+        int left = 0, maxFreqChar = 0, bestLen = 0;
 
         for (int right = 0; right < s.length(); right++) {
             int idx = s.charAt(right) - 'A';
             freq[idx]++;
+            // most frequent char in window
             maxFreqChar = Math.max(maxFreqChar, freq[idx]);
 
             while ((right - left + 1) - maxFreqChar > k) {
