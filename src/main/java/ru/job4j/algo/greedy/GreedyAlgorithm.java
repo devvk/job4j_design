ae3b4fce3794c260b6c6559ffc2ae8b8dfd67a4e
devvk <info@sekretov.net>
2026-02-23 08:52:23 +0100

    Жадный алгоритм

--- src/main/java/ru/job4j/algo/greedy/GreedyAlgorithm.java
+++ src/main/java/ru/job4j/algo/greedy/GreedyAlgorithm.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GreedyAlgorithm {
+
+    /**
+     * Вычисляет минимальное количество монет для сдачи. У нас есть монеты номиналом 1, 5, 10, 25 и 50 центов.
+     *
+     * @param amount сумма монет
+     * @return список монет для сдачи
+     */
     public static List<Integer> getChange(int amount) {
         int[] coins = {50, 25, 10, 5, 1};
         List<Integer> result = new ArrayList<>();
