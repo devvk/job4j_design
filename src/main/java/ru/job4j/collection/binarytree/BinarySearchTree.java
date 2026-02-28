06d3fb807ed505a6443667af6c4b09c1f941c4f9
devvk <info@sekretov.net>
2026-02-28 14:08:39 +0100

    Бинарное дерево поиска. Методы добавления, поиска и обхода дерева. [#504983]

---
+++ src/main/java/ru/job4j/collection/binarytree/BinarySearchTree.java
@@ -0,0 +1,231 @@
+package ru.job4j.collection.binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class BinarySearchTree<T extends Comparable<T>> {
+
+    private Node root;
+
+    public boolean put(T key) {
+        boolean result;
+        if (Objects.isNull(root)) {
+            root = new Node(key);
+            result = true;
+        } else {
+            result = put(root, key);
+        }
+        return result;
+    }
+
+    /**
+     * Добавляет узел по ключу.
+     *
+     * @param key ключ, который нужно добавить
+     * @return true, если узел добавлен, иначе false (если ключ уже существует)
+     */
+    private boolean put(Node node, T key) {
+        int compare = key.compareTo(node.key);
+        if (compare == 0) {
+            return false;
+        }
+        if (compare < 0) {
+            if (node.left == null) {
+                node.left = new Node(key);
+                return true;
+            }
+            return put(node.left, key);
+        }
+        if (node.right == null) {
+            node.right = new Node(key);
+            return true;
+        } else {
+            return put(node.right, key);
+        }
+    }
+
+    /**
+     * Проверяет наличие узла по ключу.
+     *
+     * @param key ключ, который нужно найти
+     * @return true, если узел найден, иначе false
+     */
+    public boolean contains(T key) {
+        return find(root, key) != null;
+    }
+
+    /**
+     * Поиск узла по ключу.
+     *
+     * @param node узел, с которого начинается поиск
+     * @param key  ключ, который нужно найти
+     * @return найденный узел, иначе null
+     */
+    private Node find(Node node, T key) {
+        if (node == null) {
+            return null;
+        }
+        int compare = key.compareTo(node.key);
+        if (compare == 0) {
+            return node;
+        }
+        if (compare < 0) {
+            return find(node.left, key);
+        }
+        return find(node.right, key);
+    }
+
+    public boolean remove(T key) {
+        /* Метод будет реализован в следующих уроках */
+        return false;
+    }
+
+    /**
+     * Возвращает список ключей в симметричном порядке (лево -> корень -> право).
+     *
+     * @return список ключей в отсортированном порядке
+     */
+    public List<T> inSymmetricalOrder() {
+        List<T> list = new ArrayList<>();
+        Node node = root;
+        return inSymmetricalOrder(node, list);
+    }
+
+    /**
+     * Выполняет симметричный обход поддерева (лево -> корень -> право).
+     *
+     * @param localRoot корень поддерева
+     * @param list      список для добавления ключей
+     * @return список ключей
+     */
+    private List<T> inSymmetricalOrder(Node localRoot, List<T> list) {
+        if (localRoot != null) {
+            inSymmetricalOrder(localRoot.left, list);
+            list.add(localRoot.key);
+            inSymmetricalOrder(localRoot.right, list);
+        }
+        return list;
+    }
+
+    /**
+     * Возвращает список ключей в прямом порядке (корень -> левый -> правый).
+     *
+     * @return список ключей в порядке pre-order
+     */
+    public List<T> inPreOrder() {
+        List<T> list = new ArrayList<>();
+        Node node = root;
+        return inPreOrder(node, list);
+    }
+
+    /**
+     * Выполняет прямой обход поддерева (корень -> левый -> правый).
+     *
+     * @param localRoot корень поддерева
+     * @param list      список для накопления результата
+     * @return список ключей
+     */
+    private List<T> inPreOrder(Node localRoot, List<T> list) {
+        if (localRoot != null) {
+            list.add(localRoot.key);
+            inPreOrder(localRoot.left, list);
+            inPreOrder(localRoot.right, list);
+        }
+        return list;
+    }
+
+    /**
+     * Возвращает список ключей в обратном порядке (левый -> правый -> корень).
+     *
+     * @return список ключей в порядке post-order
+     */
+    public List<T> inPostOrder() {
+        List<T> list = new ArrayList<>();
+        Node node = root;
+        return inPostOrder(node, list);
+    }
+
+    /**
+     * Выполняет обратный обход поддерева (левый -> правый -> корень).
+     *
+     * @param localRoot корень поддерева
+     * @param list      список для накопления результата
+     * @return список ключей
+     */
+    private List<T> inPostOrder(Node localRoot, List<T> list) {
+        if (localRoot != null) {
+            inPostOrder(localRoot.left, list);
+            inPostOrder(localRoot.right, list);
+            list.add(localRoot.key);
+        }
+        return list;
+    }
+
+    public T minimum() {
+        return Objects.nonNull(root) ? minimum(root).key : null;
+    }
+
+    /**
+     * Возвращает узел с минимальным ключом, начиная поиск с переданного узла.
+     *
+     * @param node узел, с которого начинается поиск
+     * @return узел с минимальным ключом
+     */
+    private Node minimum(Node node) {
+        // минимальный всегда самый левый
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    public T maximum() {
+        return Objects.nonNull(root) ? maximum(root).key : null;
+    }
+
+    /**
+     * Возвращает узел с максимальным ключом,
+     * начиная поиск с переданного узла.
+     *
+     * @param node узел, с которого начинается поиск
+     * @return узел с максимальным ключом
+     */
+    private Node maximum(Node node) {
+        // максимальный всегда самый правый
+        if (node.right == null) {
+            return node;
+        }
+        return maximum(node.right);
+    }
+
+    @Override
+    public String toString() {
+        return PrintTree.getTreeDisplay(root);
+    }
+
+    private class Node implements VisualNode {
+        private final T key;
+        private Node left;
+        private Node right;
+
+        public Node(T key) {
+            this.key = key;
+        }
+
+        @Override
+        public VisualNode getLeft() {
+            return left;
+        }
+
+        @Override
+        public VisualNode getRight() {
+            return right;
+        }
+
+        @Override
+        public String getText() {
+            return key.toString();
+        }
+    }
+}
